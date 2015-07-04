b174196e90e89e9976e2c1d5018928b4812ccae6
ashikab <ashik.bhuiyan@yahoo.com>
2015-07-04 13:54:54 +0600

    Irrelevant comment removed.

--- UVA_Solved/UVA12149_Fenyman.cpp
+++ UVA_Solved/UVA12149_Fenyman.cpp
@@ -1,17 +1,16 @@
-
-//#include <stdio.h>
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	int n;
-//    while (scanf ("%d", &n) == 1)
-//	{
-//		if(n == 0)
-//			break;
-//		int result;
-//		result = (n * (n + 1) * (2 * n + 1)) / 6;
-//		cout << result<<endl;
-//	}
-//	return 0;
-//}
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+    while (scanf ("%d", &n) == 1)
+	{
+		if(n == 0)
+			break;
+		int result;
+		result = (n * (n + 1) * (2 * n + 1)) / 6;
+		cout << result << endl;
+	}
+	return 0;
+}
--- UVA_Solved/UVA12250_Language_Detection.cpp
+++ UVA_Solved/UVA12250_Language_Detection.cpp
@@ -1,36 +1,36 @@
-//#include<cstdio>
-//#include <iostream>
-//#include <stdio.h>
-//#include<vector>
-//#include <map>
-//# include <string.h>
-//using namespace std;
-//
-//int main()
-//{
-//	int i;
-//	for(i = 1;i <= 2000; i++)
-//	{
-//		char str[14];
-//		scanf("%s", str);
-//
-//		if(str[0] == '#')
-//			break;
-//
-//		if(strcmp(str,"HELLO") == 0)
-//			printf("Case %d: ENGLISH\n",i);
-//		else if(strcmp(str,"HOLA") == 0)
-//			printf("Case %d: SPANISH\n",i);
-//		else if(strcmp(str,"HALLO") == 0)
-//			printf("Case %d: GERMAN\n",i);
-//		else if(strcmp(str,"BONJOUR") == 0)
-//			printf("Case %d: FRENCH\n",i);
-//		else if(strcmp(str,"CIAO") == 0)
-//			printf("Case %d: ITALIAN\n",i);
-//		else if(strcmp(str,"ZDRAVSTVUJTE") == 0)
-//			printf("Case %d: RUSSIAN\n",i);
-//		else
-//			printf("Case %d: UNKNOWN\n",i);
-//	}
-//	return 0;
-//}
+#include<cstdio>
+#include <iostream>
+#include <stdio.h>
+#include<vector>
+#include <map>
+# include <string.h>
+using namespace std;
+
+int main()
+{
+	int i;
+	for(i = 1;i <= 2000; i++)
+	{
+		char str[14];
+		scanf("%s", str);
+
+		if(str[0] == '#')
+			break;
+
+		if(strcmp(str,"HELLO") == 0)
+			printf("Case %d: ENGLISH\n",i);
+		else if(strcmp(str,"HOLA") == 0)
+			printf("Case %d: SPANISH\n",i);
+		else if(strcmp(str,"HALLO") == 0)
+			printf("Case %d: GERMAN\n",i);
+		else if(strcmp(str,"BONJOUR") == 0)
+			printf("Case %d: FRENCH\n",i);
+		else if(strcmp(str,"CIAO") == 0)
+			printf("Case %d: ITALIAN\n",i);
+		else if(strcmp(str,"ZDRAVSTVUJTE") == 0)
+			printf("Case %d: RUSSIAN\n",i);
+		else
+			printf("Case %d: UNKNOWN\n",i);
+	}
+	return 0;
+}
