d949093c629dbad25b52b44ffda8f2ddb5fb90a8
ashikab <ashik.bhuiyan@yahoo.com>
2015-07-14 00:13:36 +0600

    Revert "Revert "New File Added""
    This reverts commit f3cc834b93fe82a47dfc33127f2308956c8919b8.

---
+++ UVA_UnSolved/UVA495_FibonacciFreeze.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//#include <cstdio>
+//#include <algorithm>
+//#include <math.h>
+//using namespace std;
+//
+//int fib(int num)
+//{
+//	int number;
+//
+//	if(num == 0)
+//		return 0;
+//	else if(num == 1)
+//		return 1;
+//	else
+//	{
+//		number = fib(num - 1) + fib(num - 2);
+//		return number;
+//	}
+//}
+//int main() 
+//{
+//	int num, arr[5001];
+//	for(int k = 0;k <= 5000;k++)
+//	{
+//		arr[k] = fib(k);
+//	}
+//
+//	while(scanf("%d",&num) == 1)
+//		printf("The Fibonacci number for %d is %d\n", num, arr[num]);
+//    return 0;
+//}
